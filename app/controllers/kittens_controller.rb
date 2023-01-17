ddc253a18c46aa3b307298383a09477ad59147dc
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-17 02:03:02 -0800

    Build KittensController and :kittens routes
    Build routes for all 7 RESTful actions.

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,27 @@
+class KittensController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
